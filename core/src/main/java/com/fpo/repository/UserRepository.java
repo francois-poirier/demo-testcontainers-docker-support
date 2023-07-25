155b6e14763a8f02f3dc43b51751124e1c8426d6
François Poirier <fpoirier2002@gmail.com>
2023-07-25 06:36:45 +0000

    fix User

--- core/src/main/java/com/fpo/repository/UserRepository.java
+++ core/src/main/java/com/fpo/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.fpo.repository;
 
 import com.fpo.entity.User;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
